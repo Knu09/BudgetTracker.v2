aabdc3fb199e4712d764c56faa230ca7d0e39dc6
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-25 21:36:45 +0800

    add: dynamic htmx route in import page

--- web/templates/components/sidebar.php
+++ web/templates/components/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="w-[300px] duration-300 overflow-hidden py-4 border-e border-line bg-white" id="sidebar">
     <div>
         <div class="px-4 pb-2">
-            <img src="../../static/images/BRIANlysis.png" width="157" height="33" />
+            <img src="web/static/images/BRIANlysis.png" width="157" height="33" />
         </div>
         <div class="horizontal-line"></div>
         <ul class="flex flex-col">
@@ -21,10 +21,15 @@
                 <a href="/home" class="side-menu-list">Budget and Expense</a>
             </li>
             <div class="horizontal-line"></div>
-            <li
-                class="flex items-center my-2 px-4 py-2 gap-5 hover:text-primary cursor-pointer hover:bg-secondary rounded-md">
+            <li class="flex items-center my-2 px-4 py-2 gap-5 hover:text-primary cursor-pointer hover:bg-secondary rounded-md">
                 <i class="fa-solid fa-download text-[32px]"></i>
-                <a href="/import" class="side-menu-list">Import/Export CSV file</a>
+                <a href="/index.php?page=import-form"
+                    hx-get="/index.php?page=import-form"
+                    hx-target="#main-content"
+                    hx-swap="innerHTML"
+                    class="side-menu-list">
+                    Import/Export CSV file
+                </a>
             </li>
         </ul>
     </div>
